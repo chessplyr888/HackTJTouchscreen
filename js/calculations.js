869b1e82a4cd3a272bda93396b6e15ff4f6c8509
johnjp15 <johnjp15@gmail.com>
2015-05-16 19:50:26 -0400

    Distance formula
    Added the main distance formula from point to plane code.
    Finished equation of plane code, which returns plane in an associative
    array.

--- js/calculations.js
+++ js/calculations.js
@@ -1,4 +1,9 @@
-//returns cross product vector normal to vectors1 and vector2
+//returns scalar dot product of two vectors v1 and v2
+function getDotProduct(v1, v2)	{
+	return v1.i * v2.i + v1.j * v2.j + v1.k * v2.k;
+}
+
+//returns cross product vector normal to v1 and v2
 function getCrossProduct(v1, v2) {
 	i = (v1.j * v2.z - v2.j * v1.k);
 	j = (v2.i * v1.k - v1.i * v2.k);
@@ -7,6 +12,11 @@ function getCrossProduct(v1, v2) {
 	n = {"i":i, "j":j, "k":k};
 
 	return n;
+}
+
+//returns the magnitude of a vector v
+function getVectorMagnitude(v)	{
+	return Math.sqrt(getDotProduct(v, v));
 }
 
 //subtracts the two points to get a vector pointing towards point2 from point1
@@ -20,14 +30,28 @@ function getVectorFromPoints(point1, point2)  {
 	return vector;
 }
 
-//point1, point2, point3 are arrays, each one contains xyz coordinates (e.g. point1[])
+//point1, point2, point3 are arrays, each one contains xyz coordinates (e.g. point1[0] returns x coordinate)
+//returns plane in associate array form
+//plane is analogous to a(x - x0) + b(y - y0) + c(z - z0) = 0
 function getEquationOfPlaneFromPoints(point1, point2, point3) {
 	//taking point1 as center/origin (0, 0, 0)
-	vector v1 = getVectorFromPoints(point1, point2);
-	vector v2 = getVectorFromPoints(point1, point3);
+	v1 = getVectorFromPoints(point1, point2);
+	v2 = getVectorFromPoints(point1, point3);
 
-	vector n = getCrossProduct(v1, v2);
+	n = getCrossProduct(v1, v2);
 
-	
+	plane = {"normal":n, "point":point1};
 
+	return plane;
+}
+
+//point is an array, indices correspond to x, y, z respectively (e.g. 0 is x)
+//plane is associative array containing normal vector and the center point
+function getDistanceFromPointToPlane(point, plane)	{
+	v = getVectorFromPoints(plane.point, point);
+	n = plane.normal;
+
+	distance = (Math.abs(getDotProduct(n, v)) / getVectorMagnitude(n));
+
+	return distance;
 }
