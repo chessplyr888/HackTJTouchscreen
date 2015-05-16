3ce3596569798e19be68dcdbc6092dd82eb5e7b9
chessplyr888 <chessplyr888@gmail.com>
2015-05-16 18:19:30 -0400

    4 corners complete

--- js/calibration.js
+++ js/calibration.js
@@ -1,16 +1,36 @@
 var controller = {enableGestures: true};
 
+var loc1, loc2, loc3, loc4;
+var position
+
 Leap.loop(controller, function(frame){
 	if(frame.pointables.length > 0){
 		var pointable = frame.pointables[0];
-		var direction = pointable.direction;
-		var length = pointable.length;
-		var width = pointable.width;
-		var stabilizedPosition = pointable.stabilizedTipPosition;
-		var position = pointable.tipPosition;
-		var speed = pointable.tipVelocity;
-		var touchDistance = pointable.touchDistance;
-		var zone = pointable.touchZone;
-		console.log(position)
+		position = pointable.tipPosition;
+		document.getElementById("currentPointable").innerHTML = position;
 	}
 })
+
+document.onkeypress = function(event) {
+	// console.log("keypress")
+	if(event.keyCode == 49){
+		console.log("1 pressed");
+		loc1 = position;
+		document.getElementById("location1").innerHTML = loc1;
+	}
+	else if(event.keyCode == 50){
+		console.log("2 pressed");
+		loc2 = position;
+		document.getElementById("location2").innerHTML = loc2;
+	}
+	else if(event.keyCode == 51){
+		console.log("3 pressed");
+		loc3 = position;
+		document.getElementById("location3").innerHTML = loc3;
+	}
+	else if(event.keyCode == 52){
+		console.log("4 pressed");
+		loc4 = position;
+		document.getElementById("location4").innerHTML = loc4;
+	}
+}
